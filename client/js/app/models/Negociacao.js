87782af20c85b14abc36cfa30b72ae5c65ac0fbf
Douglas Fernandes <douglasf.filho@gmail.com>
2018-05-26 19:13:26 -0300

    Parado no passo 6 da aula 2

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,35 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    
+    constructor(data, quantidade, valor) {
+        if(data === undefined)
+            this._data = new Date();
+        else
+            this._data = data;
+
+        if(quantidade === undefined)
+            this._quantidade = 1;
+        else
+            this._quantidade = quantidade;
+
+        if(valor === undefined)
+            this._valor = 0.0;
+        else
+            this._valor = valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
     }
 }
